0c1a9fccdce09567049a99c7a72c7fbe06e810e7
kevineaton603 <kevin.eaton@mymail.champlain.edu>
2020-03-24 20:04:08 -0400

    this file some how got deleted

---
+++ middleware/eventAuth.js
@@ -0,0 +1,28 @@
+const jwt = require('jsonwebtoken');
+const express = require('express');
+const EventModel = require('../models/Event');
+
+/**
+ * Used by some api endpoints to determine
+ * if there is an event associated with the request
+ * @param {express.Request} req Express Request
+ * @param {express.Response} res Express Response
+ * @param {express.NextFunction} next Express Next Function
+ */
+const eventAuth = async (req, res, next) => {
+  const { eventToken } = req.cookies;
+  if (eventToken) {
+    try {
+      const eventId = await jwt.verify(eventToken, 'secret');
+      const event = await EventModel.findById(eventId);
+      req.event = event;
+      next();
+    } catch (error) {
+      res.status(401).send({ error: 'Event Authorization Error' });
+    }
+  } else {
+    res.status(401).send({ error: 'Event Authorization Error' });
+  }
+};
+
+module.exports = eventAuth;
