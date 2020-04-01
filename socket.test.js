929b5ad660a0a1b095b80d971196fe2d27ebba34
kevineaton603 <kevin.eaton@mymail.champlain.edu>
2020-04-01 15:35:27 -0400

    socket before all working

--- socket.test.js
+++ socket.test.js
@@ -2,19 +2,34 @@ const io = require('socket.io-client');
 const http = require('http');
 const ioBack = require('socket.io');
 
+const app = require('./app');
+
+/**
+ * @type {http.Server}
+ */
+let server;
+/**
+ * @type {http.AddressInfo}
+ */
+let serverAddress;
+
+/**
+ * @type {SocketIOClient.Socket}
+ */
 let socket;
-let httpServer;
-let httpServerAddr;
+/**
+ * @type {SocketIO.Server}
+ */
 let ioServer;
 
 /**
  * Setup WS & HTTP servers
  */
-beforeAll((done) => {
-  httpServer = http.createServer().listen();
-  httpServerAddr = httpServer.listen().address();
-  ioServer = ioBack(httpServer);
-  done();
+beforeAll(async () => {
+  server = http.createServer(app).listen(process.env.PORT || 3000);
+  serverAddress = server.address();
+  ioServer = ioBack();
+  ioServer.attach(server);
 });
 
 /**
@@ -22,7 +37,7 @@ beforeAll((done) => {
  */
 afterAll((done) => {
   ioServer.close();
-  httpServer.close();
+  server.close();
   done();
 });
 
@@ -32,7 +47,7 @@ afterAll((done) => {
 beforeEach((done) => {
   // Setup
   // Do not hardcode server port and address, square brackets are used for IPv6
-  socket = io.connect(`http://[${httpServerAddr.address}]:${httpServerAddr.port}`, {
+  socket = io.connect(`http://localhost:${serverAddress.port}`, {
     'reconnection delay': 0,
     'reopen delay': 0,
     'force new connection': true,
@@ -57,7 +72,19 @@ afterEach((done) => {
 
 describe('basic socket.io example', () => {
   test('should communicate', () => new Promise((done) => {
-    // once connected, emit Hello World
+    ioServer.emit('echo', 'Hello World');
+    socket.once('echo', (message) => {
+      // Check that the message matches
+      expect(message).toBe('Hello World');
+      done();
+    });
+    ioServer.on('connection', (mySocket) => {
+      console.log(mySocket);
+      expect(mySocket).toBeDefined();
+      // once connected, emit Hello World
+    });
+  }));
+  test('should communicated', () => new Promise((done) => {
     ioServer.emit('echo', 'Hello World');
     socket.once('echo', (message) => {
       // Check that the message matches
@@ -66,6 +93,7 @@ describe('basic socket.io example', () => {
     });
     ioServer.on('connection', (mySocket) => {
       expect(mySocket).toBeDefined();
+      // once connected, emit Hello World
     });
   }));
 });
