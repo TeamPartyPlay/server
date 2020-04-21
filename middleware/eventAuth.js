f1ea8700dd0a1951d219d230b32feb071bbed25c
kevineaton603 <kevin.eaton@mymail.champlain.edu>
2020-04-20 20:35:22 -0400

    event token can now be passed through body

--- middleware/eventAuth.js
+++ middleware/eventAuth.js
@@ -10,7 +10,7 @@ const EventModel = require('../models/Event');
  * @param {express.NextFunction} next Express Next Function
  */
 const eventAuth = async (req, res, next) => {
-  const { eventToken } = req.cookies;
+  const eventToken = req.cookies.eventToken || req.body.eventToken;
   if (eventToken) {
     try {
       const { eventId } = await jwt.verify(eventToken, 'secret');
